a94d3bf907031ec29a381d56cb393616cd3ee874
ziyoushibo <ziyoushibo@gmail.com>
2019-03-23 20:30:30 +0800

    good

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask,url_for
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
-@app.route('/apple')
-@app.route('/hhww')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+name = 'guoshibo'
+movies = [
+    {'title': 'my neighbor totoro', 'year': '1988'},
+    {'title': 'dead poets society', 'year': '1989'},
+    {'title': 'a perfect world', 'year': '1993'},
+    {'title': 'leon', 'year': '1994'},
+    {'title': 'mahjong', 'year': '1996'},
+    {'title': 'swallottail butterfly', 'year': '1999'},
+    {'title': 'king if comedy', 'year': '1999'},
+    {'title': 'devils on the doorstep', 'year': '1999'},
+    {'title': 'wall-e', 'year': '2008'},
+    {'title': 'the pork if music', 'year': '2012'},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', name= name, movies= movies)
 
 
-@app.route('/test') 
-def test():
-    print(url_for('hello'))
-    return url_for('hello')
+
